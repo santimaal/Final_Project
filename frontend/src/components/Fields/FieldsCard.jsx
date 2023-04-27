f0b339e3da5ac3097d8d280c4cd3181116e8bbb5
santi <santimartinezalbert02@gmail.com>
2023-04-27 19:37:24 +0200

    udpate fieldsCard

--- frontend/src/components/Fields/FieldsCard.jsx
+++ frontend/src/components/Fields/FieldsCard.jsx
@@ -13,12 +13,6 @@ export default function FieldsCard({ field }) {
                     </figure>
                     <div className="card-header_">
                         <a href="#">{field.sport} {field.slug}</a>
-                        <button className="icon-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" display="block" id="Heart">
-                                <path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
-                            </svg>
-
-                        </button>
                     </div>
                     <div className="card-footer">
                         <div className="card-meta card-meta--views">
